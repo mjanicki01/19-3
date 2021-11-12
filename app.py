4b6d322360c81b11e998cc1e9ec8127e2302ccb4
Magda Janicki <janickimagda@gmail.com>
2021-11-12 14:43:11 -0500

    changed manually coded dict to sessions dict

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, redirect, flash, jsonify
+from flask import Flask, request, render_template, redirect, flash, jsonify, session
 from flask.helpers import url_for
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey, Question, Survey
@@ -9,7 +9,6 @@ app.config['SECRET_KEY'] = "chickens"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
-responses = []
 questions = []
 
 title = satisfaction_survey.title
@@ -24,26 +23,26 @@ for q in survey_q:
 
 @app.route('/')
 def show_form():
-    responses.clear()
+    session.clear()
     return render_template('home.html', questions = questions, title = title, instructions = instr)
 
 
 @app.route('/responses/new/<int:index>', methods=["POST"])
 def add_responses(index):
-        response = int(request.form["question"])
-        responses.append({"index": index, "question": questions[index]['question'], "choice": questions[index]['choices'][response]})
+        choice = request.form['choice']
+        session[f"{questions[index]['question']}"] = choice
         return redirect('/questions/' + str(index + 1))
 
 
 @app.route('/questions/<int:index>', methods=["GET", "POST"])
 def load_question(index):
-    if index != len(responses):
+    if index != len(session):
         flash(f"Out of sequence question")
-        return redirect(f"/questions/{len(responses)}")
+        return redirect(f"/questions/{len(session)}")
 
     if index < len(questions):
         return render_template('question.html', index = index, question = questions[index], title = title)
     elif index == len(questions):
-        return render_template('thankyou.html', title=title, responses = responses)
+        return render_template('thankyou.html', title=title)
     else:
         return render_template('error.html', title=title)
